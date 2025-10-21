68969cbf05888ddb9c1411feb18221c3006e0381
Lory Marie <lory.marie02@outlook.fr>
2025-10-21 08:14:50 -0400

    Update db.js

--- mon-api-main/config/db.js
+++ mon-api-main/config/db.js
@@ -1,18 +1,11 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config(); // charge ton .env
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5000,
+    port: process.env.DB_PORT || 3306, // ✅ port MySQL
     dialect: "mysql",
     logging: false,
   }
 );
-
-export default sequelize;
