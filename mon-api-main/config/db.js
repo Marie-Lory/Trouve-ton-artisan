e7fbf7a730e72efbfcb5141d59e5d66c5fdaec29
Lory Marie <lory.marie02@outlook.fr>
2025-10-22 15:54:23 -0400

    Update db.js

--- mon-api-main/config/db.js
+++ mon-api-main/config/db.js
@@ -1,8 +1,7 @@
 // ✅ config/db.js
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
-dotenv.config(); // charge ton .env
+dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -10,20 +9,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
   }
 );
 
-// ✅ Teste la connexion directement ici (utile sur Render)
-try {
-  await sequelize.authenticate();
-  console.log("✅ Connexion MySQL réussie !");
-} catch (error) {
-  console.error("❌ Erreur de connexion MySQL :", error);
-  process.exit(1); // stoppe le serveur en cas d'erreur
-}
-
 export default sequelize;
-
