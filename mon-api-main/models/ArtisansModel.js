d657236da22bea8100b5da1a2f16ee0a84aedd83
Lory Marie <lory.marie02@outlook.fr>
2025-10-22 15:54:36 -0400

    Update ArtisansModel.js

--- mon-api-main/models/ArtisansModel.js
+++ mon-api-main/models/ArtisansModel.js
@@ -1,79 +1,52 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
-const Artisans = sequelize.define(
-  "Artisans",
-  {
-    id_artisan: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    nom: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    specialite: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    note: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        isFloat: {
-          msg: "La note doit être un nombre ou un nombre à virgule",
-        },
-        min: 0,
-        max: 5,
-      },
-    },
-    ville: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    a_propos: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: {
-          msg: "L'email doit être valide",
-        },
-      },
-    },
-    site_web: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        isUrlOrEmpty(value) {
-          // ✅ si vide ou null, ne rien valider
-          if (value && value.trim() !== "" && !/^https?:\/\/.+/.test(value)) {
-            throw new Error("Le site web doit être une URL valide");
-          }
-        },
-      },
-      set(value) {
-        this.setDataValue("Site_Web", value && value.trim() !== "" ? value : null);
-      },
-    },
-    categorie: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    top: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
+const Artisan = sequelize.define("artisanentreprise", {
+  id_artisan: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    tableName: "artisanentreprise",
-    timestamps: false,
-  }
-);
+  nom: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  specialite: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  note: {
+    type: DataTypes.FLOAT(3,1),
+    allowNull: false,
+  },
+  ville: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  a_propos: {
+    type: DataTypes.STRING(500),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    unique: true,
+    allowNull: false,
+  },
+  site_web: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
+  categorie: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  top: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+}, {
+  timestamps: false,
+  tableName: "artisanentreprise",
+});
 
-export default Artisans;
+export default Artisan;
